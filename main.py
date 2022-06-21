718b79d028c06df11ecdf1e55ff4c23a574f1884
Pranav Malik <86750720+Pranav2625@users.noreply.github.com>
2022-06-21 22:19:16 +0000

    Added Dealer's side

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from functools import partial
 class Single_Game:
   def __init__(self, partner):
 
-    self.single_frame = Frame(width = 900, height = 500, bg = "Green")
+    self.single_frame = Frame(width = 700, height = 500, bg = "Green") # Window Frame
     self.single_frame.grid()
 
+    self.dealer_side = Label(self.single_frame, text = "Dealer", font = "Times 14 bold", # Dealer's side
+                              bg = '#733f19', fg = "White", padx = 330, pady = 12) 
+    self.dealer_side.place(x = -10, y = 0) # used place to postion the strip
 
 
 if __name__ == "__main__":
