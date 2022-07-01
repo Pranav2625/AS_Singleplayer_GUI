574dce23d887dd214ac281573e35e395d246d5e5
Pranav Malik <86750720+Pranav2625@users.noreply.github.com>
2022-07-01 22:47:56 +0000

    Updated prgm (self removed)

--- main.py
+++ main.py
@@ -2,21 +2,57 @@ from tkinter import *
 from functools import partial
 
 
-class Single_Game:
-  def __init__(self, partner):
+root = Tk()
+root.title("Single Player Mode") # Window title
 
-    self.single_frame = Frame(width = 700, height = 500, bg = "Green") # Window Frame
-    self.single_frame.grid()
+sngle_frame = Frame(root, width=700, height=500, bg="green")  # Window Frame
+sngle_frame.grid()
 
-    self.dealer_side = Label(self.single_frame, text = "Dealer", font = "Times 14 bold", # Dealer's side
-                              bg = '#733f19', fg = "White", padx = 330, pady = 12) 
-    self.dealer_side.place(x = -10, y = 0) # used place to postion the strip
+dealer_side = Label(sngle_frame, text="Dealer", font="Times 14 bold", # Dealer's Side
+                    bg="#733f19", fg="white", padx=330, pady=12)      # of the table
+dealer_side.place(x=-10, y=0)
 
-    
+sngle_creds=500  # Intial value of credits
+sngle_bets=0     # and bets
 
 
-if __name__ == "__main__":
-  root = Tk()
-  root.title("Single Player Mode")
-  screen = Single_Game(root)
-  root.mainloop()
+
+def sngle_bets_add():    # For adding bets
+  global sngle_bets      # get the intitial value
+  sngle_bet_add = sngle_bet_add_sub_var.get() # assign the add subtract intvar to a variable 
+  sngle_bets += sngle_bet_add # Add five (sngle_bet_add) to current bet value
+  sngle_bets_counter.set("Bets: ${:.2f}".format(sngle_bets)) # format the vale into the counter
+  sngle_bet_add_sub_var.set(5) # Makes sure that this variable is set to 5
+
+def sngle_bets_sub(): # For removing bets
+  global sngle_bets
+  sngle_bet_sub = sngle_bet_add_sub_var.get()
+  sngle_bets -= sngle_bet_sub # Subtract five from the current bet value
+  sngle_bets_counter.set("Bets: ${:.2f}".format(sngle_bets))
+  sngle_bet_add_sub_var.set(5)
+
+sngle_bets_counter = StringVar()  # Sets variable as a string varible
+sngle_bets_counter.set("Bets: $0") # Sets the text of the string
+sngle_bets_display = Label(sngle_frame, textvariable=sngle_bets_counter, bg="orange",
+                   font="Times 10") # Makes the bets counter display
+sngle_bets_display.place(x=580, y=100)
+
+
+sngle_creds_counter = StringVar()
+sngle_creds_counter.set("Credits: $500")
+sngle_creds_dsiplay = Label(sngle_frame, textvariable=sngle_creds_counter, bg="orange",
+                           font="Times 10") # Makes the credits counter display
+sngle_creds_dsiplay.place(x=580, y=70)
+
+
+sngle_bet_add_sub_var=IntVar()  # Assigns this as an intger variable
+sngle_bet_add_sub_var.set(5)    # Sets a numerical value
+sngle_bet_add_but=Button(sngle_frame, text="Add bets", bg="gold", bd=1, command=sngle_bets_add)
+sngle_bet_add_but.place(x=580, y=150) # Button for adding bets
+sngle_bet_sub_but=Button(sngle_frame, text="Remove bets", bg="gold", bd=1, command=sngle_bets_sub)
+sngle_bet_sub_but.place(x=580, y=180) # Button for removing bets
+
+
+
+
+root.mainloop()  # Loops the program until stopped/exited
