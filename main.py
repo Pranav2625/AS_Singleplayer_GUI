d2013513fd9011b296689823bd0ca96cc259967a
Pranav Malik <86750720+Pranav2625@users.noreply.github.com>
2022-07-04 02:00:59 +0000

    Added confirm bets button

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from functools import partial
+import random
 
+card_num_1 = random.randint(1, 13)
+card_num_2 = random.randint(1, 13)
 
 root = Tk()
 root.title("Single Player Mode") # Window title
@@ -44,13 +47,30 @@ sngle_creds_dsiplay = Label(sngle_frame, textvariable=sngle_creds_counter, bg="o
                            font="Times 10") # Makes the credits counter display
 sngle_creds_dsiplay.place(x=580, y=70)
 
-
 sngle_bet_add_sub_var=IntVar()  # Assigns this as an intger variable
 sngle_bet_add_sub_var.set(5)    # Sets a numerical value
 sngle_bet_add_but=Button(sngle_frame, text="Add bets", bg="gold", bd=1, command=sngle_bets_add)
 sngle_bet_add_but.place(x=580, y=150) # Button for adding bets
 sngle_bet_sub_but=Button(sngle_frame, text="Remove bets", bg="gold", bd=1, command=sngle_bets_sub)
 sngle_bet_sub_but.place(x=580, y=180) # Button for removing bets
+
+def sngle_cnfrm_bet():
+  sngle_bet_add_but.config(state=DISABLED)
+  sngle_bet_sub_but.config(state=DISABLED)
+
+sngle_confirm_bet=Button(sngle_frame, text="Confirm bets", bg="orange", command=sngle_cnfrm_bet)
+sngle_confirm_bet.place(x=580, y=220)
+
+
+sngle_hit_button=Button(sngle_frame, text="Hit", bg="white", bd=1)  # Hit button
+sngle_hit_button.place(x=250, y=335)
+
+sngle_stay_button=Button(sngle_frame, text="Stay", bg="white", bd=1)  # Stay button
+sngle_stay_button.place(x=375, y=335)
+
+sngle_double_button=Button(sngle_frame, text="Double", bg="white", bd=1)  # Double button
+sngle_double_button.place(x=300, y=375)
+
 
 
 
